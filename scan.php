88c4487d28f45dd9e5a2798622c4151ee92ceccb
marcell <marcell@marcellkovacs.com>
2017-06-13 21:10:00 +0100

    Fixed path

--- scan.php
+++ scan.php
@@ -1,5 +1,5 @@
 <?php
-$file_name = exec("python3 scanner.py --mode={$_POST['mode']} --resolution={$_POST['resolution']} --compression={$_POST['compression']}");
+$file_name = exec("python3 /bin/scanner.py --mode={$_POST['mode']} --resolution={$_POST['resolution']} --compression={$_POST['compression']}");
 $Obj->file_name = $file_name;
 echo json_encode($Obj, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
 ?>

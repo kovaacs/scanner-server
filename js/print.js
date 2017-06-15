23ea9bb7b8822eb1bbdb65a752ec8cf56e2ddeb1
marcell <marcell@marcellkovacs.com>
2017-06-15 21:04:56 +0100

    GUI/AJAX changes

--- js/print.js
+++ js/print.js
@@ -1,7 +1,7 @@
 const AJAX = {
-    getBlob: function(URL, callback) {
+    getBlob: function (URL, callback) {
         const XMLreq = new XMLHttpRequest();
-        XMLreq.onreadystatechange = function() {
+        XMLreq.onreadystatechange = function () {
             if (XMLreq.readyState === 4) {
                 if (XMLreq.status === 200) {
                     callback(XMLreq.response);
@@ -12,9 +12,9 @@ const AJAX = {
         XMLreq.open("GET", URL, true);
         XMLreq.send();
     },
-    post: function(URL, data, callback) {
+    post: function (URL, data, callback) {
         const XMLreq = new XMLHttpRequest();
-        XMLreq.onreadystatechange = function() {
+        XMLreq.onreadystatechange = function () {
             if (XMLreq.readyState === 4) {
                 if (XMLreq.status === 200) {
                     callback(XMLreq.responseText);
@@ -25,7 +25,7 @@ const AJAX = {
         XMLreq.open("POST", URL, true);
         XMLreq.send(data);
     },
-    download: function(inBlob, fileName, MIMEtype) {
+    download: function (inBlob, fileName, MIMEtype, callback) {
         let a = document.createElement("a"),
             url;
         inBlob.type = MIMEtype;
@@ -35,26 +35,39 @@ const AJAX = {
         a.download = fileName;
         a.click();
         window.URL.revokeObjectURL(url);
-
+        if (callback) callback();
     }
 };
-(function() {
-    window.addEventListener("load", function() {
+(function () {
+    window.addEventListener("load", function () {
         const formPost = document.getElementById("form_post");
         const buttonPost = document.getElementById("button_post");
-        formPost.addEventListener("submit", function(element) {
+        const inputResolution = document.getElementById("resolution");
+        const sliderResolution = document.getElementById("slider-resolution");
+        document.getElementById("button_post");
+        formPost.addEventListener("submit", function (element) {
             element.preventDefault();
             const formData = new FormData(formPost);
-            AJAX.post("scan.php", formData, function(resp) {
+            AJAX.post("scan.php", formData, function (resp) {
                 const parsedJSON = JSON.parse(resp);
-                AJAX.getBlob(parsedJSON.file_name, function(resp) {
-                    AJAX.download(resp, parsedJSON.file_name.split("/")[1], "application/pdf");
+                AJAX.getBlob(parsedJSON.file_name, function (resp) {
+                    AJAX.download(resp, parsedJSON.file_name.split("/")[1], "application/pdf", function () {
+                        buttonPost.removeAttribute("disabled");
+                        buttonPost.value = "Scan";
+                    });
                 });
-                buttonPost.removeAttribute("disabled");
-                buttonPost.value = "Scan";
             });
             buttonPost.setAttribute("disabled", true);
             buttonPost.value = "Please wait";
-        })
-    })
+        });
+        inputResolution.addEventListener("keyup", function () {
+            if (inputResolution.validity.valid && inputResolution.value !== "") {
+                sliderResolution.MaterialSlider.change(inputResolution.value);
+            }
+        });
+        sliderResolution.addEventListener("input", function () {
+            inputResolution.value = sliderResolution.value;
+        });
+
+    });
 })();
